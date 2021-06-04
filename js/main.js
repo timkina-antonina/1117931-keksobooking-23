21eedb289b0fa1c2ee74ed4cde807240f6c1ccce
atimkina <atimkina@gemsdev.com>
2021-06-04 12:54:05 +0600

    исправление ошибок
    линтер

--- js/main.js
+++ js/main.js
@@ -5,17 +5,19 @@ const getRandomIntegerInclusive = function (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  return "Минимальное значение диапазона должно быть меньше максимального. Или диапазон не содержит целых значений.";
+  return 'Минимальное значение диапазона должно быть меньше максимального. Или диапазон не содержит целых значений.';
 };
 
 const getRandomFloatInclusive = function (min, max, decimalPlaces) {
+	
   if (min < max) {
     const randomFloatInclusive = Math.random() * (max - min) + min;
     return randomFloatInclusive.toFixed(decimalPlaces);
   }
 
-  return "Минимальное значение диапазона должно быть меньше максимального.";
+  return 'Минимальное значение диапазона должно быть меньше максимального.';
 };
 
 getRandomIntegerInclusive(1.4, 2.5);
 getRandomFloatInclusive(0.5, 3, 2);
+
